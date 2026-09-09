03419f09509dd29df9e032ec822d2457be6ee252
Frederic Charette <fredericcharette@gmail.com>
2026-09-09 19:06:59 -0400

    added node install script
    Add script to install Node.js using NVM if not already installed.

---
+++ src/install/node.sh
@@ -0,0 +1,6 @@
+if ! command -v node &> /dev/null; then
+    wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.7/install.sh | PROFILE=/dev/null bash
+fi
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
