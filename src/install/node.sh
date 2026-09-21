8f8c0e7cccfd836d1750bde820c56cee941375e5
Frederic Charette <fredericcharette@gmail.com>
2026-09-21 00:24:56 -0400

    modified node install to be global, fixed bad hardening rule

--- src/install/node.sh
+++ src/install/node.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 if ! command -v node &> /dev/null; then
-    wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.7/install.sh | PROFILE=$NB_DIR/install/pre.sh bash
-    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-    nvm install --lts
+    # NVM is user-specific and not ideal for our use-case, we will rarely need multiple versions installed
+    # wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.7/install.sh | PROFILE=$NB_DIR/install/pre.sh bash
+    # export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+    # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+    # nvm install --lts
+
+    # Yuck...we need curl for this??
+    apt-get install curl -y -qq 
+    curl -fsSL https://deb.nodesource.com/setup_26.x -o nodesource_setup.sh
+    sudo -E bash nodesource_setup.sh
+    apt-get install nodejs -y -qq
 fi
